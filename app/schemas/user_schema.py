b165ab3ee01a821df7b70e76dc3423bc178a068e
Precious Steve <precioussteve01@gmail.com>
2024-09-21 11:05:08 +0100

    update reset password schema validation

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -12,6 +12,28 @@ class ResetForgotPassword(BaseModel):
     new_password: str
     confirm_password: str
     
+    @field_validator('new_password')
+    def validate_password(cls, value: str) -> str:
+        if len(value) < 8:
+            raise ValueError('Password must be at least 8 characters long')
+        if not any(c.islower() for c in value):
+            raise ValueError('Password must contain at least one lowercase letter')
+        if not any(c.isupper() for c in value):
+            raise ValueError('Password must contain at least one uppercase letter')
+        if not any(c.isdigit() for c in value):
+            raise ValueError('Password must contain at least one digit')
+        if not any(c in '!@#$%^&*()-_+=' for c in value):
+            raise ValueError('Password must contain at least one special character')
+        return value
+    
+    
+    @field_validator('confirm_password')
+    def passwords_match(cls, value: str, info: Any) -> str:
+        if 'new_password' in info.data and value != info.data['new_password']:
+            raise ValueError('New Password and confirm password do not match')
+        return value
+    
+    
 class SuccessMessage(BaseModel):
     success: bool
     status_code: int
